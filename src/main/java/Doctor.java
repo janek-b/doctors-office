f87e5c9f5b7ca0e1907b58478f3693f9dca13289
Janek Brandt and kaya winn <janek@>
2017-03-28 09:33:44 -0700

    add initial project directories and files

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,9 @@
+import org.sql2o.*;
+
+public class Doctor {
+
+  public Doctor() {
+
+  }
+
+}
---
+++ src/main/java/Specialty.java
@@ -0,0 +1,9 @@
+import org.sql2o.*;
+
+public class Specialty {
+
+  public Specialty() {
+
+  }
+
+}
